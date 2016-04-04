24cc68fa83541875b0c902976a65c9f14b0b3f57
zachnewburgh <zach.newburgh@gmail.com>
2016-04-03 22:25:31 -0400

    Done.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// We should wait for the page to load before running our ajax request
+// We should wait for the page to load before running our ajax request.
 $(document).ready(function(){
   // Now we start the Ajax Get request. The first parameter is the URL with the data.
   // The second parameter is a function that handles the response.
